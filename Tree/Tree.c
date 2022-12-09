af61c1e3dcd07d264c3d381971321ef206d0a30c
叶珲 <365526026@qq.com>
2022-12-09 12:10:47 +0800

    1
    1

--- Tree/Tree.c
+++ Tree/Tree.c
@@ -2,5 +2,44 @@
 #include<stdlib.h>
 #include<string.h>
 #include <stdbool.h>
+#define M 100
+struct tree //定义树结点结构
+{
+    char data;
+    struct tree *left;
+    struct tree *right;
+};
 
-typedef
+typedef struct tree treenode,*btree;
+/*定义队列*/
+btree que[M];
+int front = 0 , rear = 0;
+btree root = NULL;
+int count = 0;
+/*先根遍历方法创建二叉树*/
+btree createtree(){
+    btree pdnode;
+    char ch;
+    scanf("%c",&ch);
+        if(ch == '@')
+            pdnode = NULL; /*遇到‘@’，不分配新结点*/
+        else{
+            pdnode = (btree) malloc (sizeof(treenode));
+            if(pdnode==NULL){
+                printf("Out of space ! \n");
+                return pdnode;
+            }
+            pdnode->data = ch;
+            pdnode->left = createtree();
+            pdnode->right = createtree();
+            }
+    return pdnode;
+}
+
+
+int main(){
+    printf("请输入带@的二叉树序列:");
+    root = createtree();
+    printf("%c \n",root->right->data);
+    return 0;
+}
